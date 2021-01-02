5d16d21e5cc63c653d08bf8ec95bdd27dad846af
lucashec <lucashenriqueev@gmail.com>
2021-01-02 17:06:56 -0300

    adding search

--- buscabelo/accounts/views.py
+++ buscabelo/accounts/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from .models import Provider
+from django.db.models import Q
 
-# Create your views here.
+def index(request):
+  return render(request, 'accounts/home.html')
+def searchView(request):
+  if request.method == 'GET':
+    search = request.GET.get('search')
+    provider = Provider.objects.all().filter(Q(username__icontains=search) | Q(description__icontains=search))
+    return render(request, 'accounts/search.html', {'providers': provider})
+
